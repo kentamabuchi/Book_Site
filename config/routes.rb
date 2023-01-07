de289de0a4ce48c65bca8c1048ed06d91c51aef3
馬渕健太 <mabuchikenta@mabuchikentanoMacBook-Pro.local>
2023-01-08 03:35:58 +0900

    pblic-homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
