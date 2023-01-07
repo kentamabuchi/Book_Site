9908da0c33e13ea66ced220fa2e5694ecc641f18
馬渕健太 <mabuchikenta@mabuchikentanoMacBook-Pro.local>
2023-01-07 20:01:00 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BookSite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.hosts << 'https://book-site-38ma.onrender.com'
+    config.hosts << 'book-site-38ma.onrender.com'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
