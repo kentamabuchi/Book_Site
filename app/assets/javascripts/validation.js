3e30b5f25c922bc102b40909546c8f6c31444fd7
馬渕健太 <mabuchikenta@mabuchikentanoMacBook-Pro.local>
2023-02-13 21:50:13 +0900

    admin-admins

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -9,27 +9,28 @@
       this.form = document.getElementById(params.form);
       this.formText = document.getElementsByClassName(params.formText);
 
-      // console.log(this.submitBtn);
-      // console.log(this.form);
-
       if (!this.submitBtn){ return false;}
+
+      if(params.form.includes("SignIn") && this.formText[0].value !== "" && this.formText[1].value === ""){
+        this.formText[0].nextElementSibling.textContent = "メールアドレスかパスワードが違います";
+        this.formText[0].classList.add("error__form");
+        this.formText[1].classList.add("error__form");
+      }
 
 
       this.submitBtn.addEventListener('click', (e)=>{
+
         e.preventDefault();
 
         let active = true;
 
         for(let i = 0; i < this.formText.length; i++){
           if(this.formText[i].value === ""){
-            // this.formText[i].after(document.createElement('p'));
             this.formText[i].nextElementSibling.textContent = "入力されていません";
             this.formText[i].classList.add("error__form");
             this.formText[i].nextElementSibling.classList.add("error__text");
-            // console.log(i);
 
             active = false;
-            // console.log(this.form.length);
           }else if(this.formText[i].value !== "" && active === false){
             this.formText[i].nextElementSibling.textContent = "";
             this.formText[i].classList.remove("error__form");
@@ -41,51 +42,37 @@
             this.formText[i].nextElementSibling.classList.remove("error__text");
           }
         }
-        console.log(active);
 
-        // this.action();
-        // this.formText.map(function(text){
-        //   if(text.value === "" && this.count === 0){
-        //     this.formText[i].after(document.createElement('p'));
-        //     this.formText[i].nextElementSibling.textContent = "入力されていません";
-        //   }
-        // });
-
-        if(active === true){
+        if(active == true){
           this.form.submit();
         }
-
       });
-
-
     },
+
     action: function(){
       for(let i = 0; i < this.formText.length; i++){
         if(this.formText[i].value === "" && this.count === 0){
-          // this.formText[i].after(document.createElement('p'));
           this.formText[i].nextElementSibling.textContent = "入力されていません";
           console.log(i);
-          // console.log(this.form.length);
         }
       }
-      // this.count++;
-
-
-      // this.formText.map(function(text){
-      //   if(text.value === "" && this.count === 0){
-      //     this.formText[i].after(document.createElement('p'));
-      //     this.formText[i].nextElementSibling.textContent = "入力されていません";
-      //   }
-
-      // });
-
     }
 
   }
-  validation.init ({
-    submitBtn: "bookNewBtn",
-    form: "bookNewForm",
-    formText: "validation__form"
-  });
+
+  if(location.href.includes('books/new')){
+    validation.init ({
+      submitBtn: "bookNewBtn",
+      form: "bookNewForm",
+      formText: "validation__form"
+    });
+  }else if(location.href.includes('admins/sign_in')){
+    validation.init ({
+      submitBtn: "adminSignInBtn",
+      form: "adminSignInForm",
+      formText: "validation__form"
+    });
+  }
+
 }
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :get_path
+
+  def get_path
+    @path = request.fullpath
+  end
+
+
+
+  protected
+    
+    def configure_permitted_parameters
+        if resource_class == User
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name,
+                                                              :email,
+                                                              :created_at,
+                                                              :updated_at])
+          devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+          devise_parameter_sanitizer.permit(:account_ipdate, keys: [:name,
+                                                                    :email,
+                                                                    :created_at,
+                                                                    :updated_at])
+        elsif resource_class == Admin
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name,
+                                                             :email,
+                                                             :created_at,
+                                                             :updated_at])
+          devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+          devise_parameter_sanitizer.permit(:account_ipdate, keys: [:name,
+                                                                    :email,
+                                                                    :created_at,
+                                                                    :updated_at])
+        end
+    end
+
+    def after_sign_in_path_for(resource)
+      case resource
+          when Admin
+            homes_top_path
+          when User
+            books_path
+      end
+    end
 end
