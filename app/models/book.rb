4f8d01a6fe975fc6b3e8e221cc33472ce5d9c803
馬渕健太 <mabuchikenta@mabuchikentanoMacBook-Pro.local>
2023-02-24 20:12:01 +0900

    public-books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
 
   validates :name, presence: true
   validates :writer, presence: true
+  attachment :image
+  belongs_to :category
+  belongs_to :genre
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '443f0508d771cf1eab8a6a8fe0ff29eda5c7175070518fe31dfd558a095c70fa2c622d3dc33313d997e56020d19045407381f1e06854346e3d47a43c4a82dcdd'
